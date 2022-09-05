018f0101bf0469b47dd3820bb9fb97ae16f612e2
Agustin Mattiussi <amattiussi@itba.edu.ar>
2022-09-05 12:11:18 -0300

    Add Experience class with its respective DAO and service (no testing)

---
+++ paw-webapp/interfaces/src/main/java/ar/edu/itba/paw/interfaces/services/ExperienceService.java
@@ -0,0 +1,13 @@
+package ar.edu.itba.paw.interfaces.services;
+
+import ar.edu.itba.paw.interfaces.persistence.ExperienceDao;
+import ar.edu.itba.paw.models.Experience;
+
+import java.sql.Date;
+import java.util.Optional;
+
+public interface ExperienceService {
+    Experience create(long userId, Date from, Date to, String enterpriseName, String position, String description);
+
+    Optional<Experience> findById(long experienceId);
+}
--- paw-webapp/persistence/src/test/resources/init.sql
+++ paw-webapp/persistence/src/test/resources/init.sql
@@ -3,4 +3,6 @@ INSERT INTO rubro (nombre) VALUES ('testCategory');
 INSERT INTO aptitud (descripcion) VALUES ('testSkill');
 INSERT INTO usuario (nombre, email, contrasenia, descripcion, idRubro, ubicacion, posicionActual, educacion) VALUES ('John Lennon', 'johnlennon@gmail.com', 'imagineAPassword', null, null, null, null, null);
 INSERT INTO empresa (nombre, email, contrasenia, descripcion, idRubro, ubicacion) VALUES ('Empresaurio', 'empresaurio@gmail.com', '12345678', null, null, null);
+-- INSERT INTO experienca (idEmpresa, fechaDesde, fechaHasta, empresa, posicion, descipcion) VALUES (1, '2010-10-10', null, null, 'CEO', null);
+
 
