4bf2d48858f8bae33d67018bef80f2e81d62a86d
Julian Sasso <jsasso@itba.edu.ar>
2022-10-01 19:45:42 -0300

    Optimize JobOfferSkillJdbcDao queries

--- paw-webapp/persistence/src/main/java/ar/edu/itba/paw/persistence/JobOfferSkillJdbcDao.java
+++ paw-webapp/persistence/src/main/java/ar/edu/itba/paw/persistence/JobOfferSkillJdbcDao.java
@@ -1,17 +1,23 @@
 package ar.edu.itba.paw.persistence;
 
+import ar.edu.itba.paw.interfaces.persistence.CategoryDao;
 import ar.edu.itba.paw.interfaces.persistence.JobOfferDao;
 import ar.edu.itba.paw.interfaces.persistence.JobOfferSkillDao;
 import ar.edu.itba.paw.interfaces.persistence.SkillDao;
+import ar.edu.itba.paw.models.Category;
 import ar.edu.itba.paw.models.JobOffer;
 import ar.edu.itba.paw.models.Skill;
+import ar.edu.itba.paw.persistence.exceptions.CategoryNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.util.*;
+
+import static ar.edu.itba.paw.persistence.SkillJdbcDao.SKILL_MAPPER;
 
 @Repository
 public class JobOfferSkillJdbcDao implements JobOfferSkillDao {
@@ -19,18 +25,45 @@ public class JobOfferSkillJdbcDao implements JobOfferSkillDao {
     private static final String JOB_OFFER_SKILL_TABLE = "aptitudOfertaLaboral";
     private static final String JOB_OFFER_ID = "idOferta";
     private static final String SKILL_ID = "idAptitud";
+    private static final String JOB_OFFER_TABLE = "ofertaLaboral";
+    private static final String JOB_OFFER_TABLE_ID = "id";
+    private static final String JOB_OFFER_TABLE_ENTERPRISE_ID = "idEmpresa";
+    private static final String JOB_OFFER_TABLE_CATEGORY_ID = "idRubro";
+    private static final String JOB_OFFER_TABLE_POSITION = "posicion";
+    private static final String JOB_OFFER_TABLE_DESCRIPTION = "descripcion";
+    private static final String JOB_OFFER_TABLE_SALARY = "salario";
+    private static final String JOB_OFFER_TABLE_MODALITY = "modalidad";
+
     private final SkillDao skillDao;
     private final JobOfferDao jobOfferDao;
     private final JdbcTemplate template;
     private final SimpleJdbcInsert insert;
+    private CategoryDao categoryDao;
+
+    private final RowMapper<JobOffer> JOB_OFFER_MAPPER = ((resultSet, rowNum) -> {
+        long categoryID = resultSet.getLong(JOB_OFFER_TABLE_CATEGORY_ID);
+        Category category = null;
+
+        if(categoryID != 0)
+            category = categoryDao.findById(categoryID).orElseThrow(CategoryNotFoundException::new);
+
+        return new JobOffer(resultSet.getLong(JOB_OFFER_TABLE_ID),
+                resultSet.getLong(JOB_OFFER_TABLE_ENTERPRISE_ID),
+                category,
+                resultSet.getString(JOB_OFFER_TABLE_POSITION),
+                resultSet.getString(JOB_OFFER_TABLE_DESCRIPTION),
+                resultSet.getBigDecimal(JOB_OFFER_TABLE_SALARY),
+                resultSet.getString(JOB_OFFER_TABLE_MODALITY));
+    });
 
     @Autowired
-    public JobOfferSkillJdbcDao(final DataSource ds, final SkillDao skillDao, final JobOfferDao jobOfferDao){
+    public JobOfferSkillJdbcDao(final DataSource ds, final SkillDao skillDao, final JobOfferDao jobOfferDao, CategoryDao categoryDao){
         this.template = new JdbcTemplate(ds);
         this.insert = new SimpleJdbcInsert(ds)
                 .withTableName(JOB_OFFER_SKILL_TABLE);
         this.skillDao = skillDao;
         this.jobOfferDao = jobOfferDao;
+        this.categoryDao = categoryDao;
     }
 
     @Override
@@ -52,22 +85,15 @@ public class JobOfferSkillJdbcDao implements JobOfferSkillDao {
     }
 
     private List<Long> getJobOfferIDsWithSkill(long skillID){
-        return template.query("SELECT " + JOB_OFFER_ID + " FROM " + JOB_OFFER_SKILL_TABLE + " WHERE " + SKILL_ID + " = ?",
+        return template.query("SELECT idOferta FROM aptitudOfertaLaboral WHERE idAptitud = ?",
                 new Object[]{ skillID }, (resultSet, rowNum) ->
             resultSet.getLong(JOB_OFFER_ID));
     }
 
     @Override
     public List<JobOffer> getJobOffersWithSkill(long skillID) {
-        List<Long> jobOfferIDs = getJobOfferIDsWithSkill(skillID);
-        List<JobOffer> jobOfferList = new ArrayList<>();
-
-        for (Long id : jobOfferIDs) {
-            Optional<JobOffer> currentJobOffer = jobOfferDao.findById(id);
-            currentJobOffer.ifPresent(jobOfferList::add);
-        }
-
-        return jobOfferList;
+        return template.query("SELECT * FROM aptitudOfertaLaboral aol JOIN ofertaLaboral ol ON aol.idOferta = ol.id WHERE aol.idAptitud = ?",
+                new Object[]{ skillID }, JOB_OFFER_MAPPER);
     }
 
     @Override
@@ -77,21 +103,15 @@ public class JobOfferSkillJdbcDao implements JobOfferSkillDao {
     }
 
     private List<Long> getSkillIDsForJobOffer(long jobOfferID){
-        return template.query("SELECT " + SKILL_ID + " FROM " + JOB_OFFER_SKILL_TABLE + " WHERE " + JOB_OFFER_ID + " = ?",
+        return template.query("SELECT idAptitud FROM aptitudOfertaLaboral WHERE idOferta = ?",
                 new Object[]{ jobOfferID }, (resultSet, rowNum) ->
             resultSet.getLong(SKILL_ID));
     }
 
     @Override
     public List<Skill> getSkillsForJobOffer(long jobOfferID) {
-        List<Long> skillIDs = getSkillIDsForJobOffer(jobOfferID);
-        List<Skill> skillList = new ArrayList<>();
-
-        for (Long id : skillIDs) {
-            Optional<Skill> currentSkill = skillDao.findById(id);
-            currentSkill.ifPresent(skillList::add);
-        }
-
-        return skillList;
+        return template.query("SELECT a.id, a.descripcion FROM aptitudOfertaLaboral aol JOIN " +
+                "aptitud a ON aol.idAptitud = a.id WHERE aol.idOferta = ?",
+                new Object[]{jobOfferID}, SKILL_MAPPER);
     }
 }
