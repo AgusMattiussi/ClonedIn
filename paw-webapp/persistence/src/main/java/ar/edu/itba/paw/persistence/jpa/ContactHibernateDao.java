9ded19255d7b2df19488000b59b2e46640894b15
Julian Sasso <jsasso@itba.edu.ar>
2022-10-26 11:32:24 -0300

    Begin Implementation of ContactHibernateDao

---
+++ paw-webapp/persistence/src/main/java/ar/edu/itba/paw/persistence/jpa/ContactHibernateDao.java
@@ -0,0 +1,101 @@
+package ar.edu.itba.paw.persistence.jpa;
+
+import ar.edu.itba.paw.interfaces.persistence.ContactDao;
+import ar.edu.itba.paw.models.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ContactHibernateDao implements ContactDao {
+
+    @Override
+    public void addContact(long enterpriseID, long userID, long jobOfferID) {
+
+    }
+
+    @Override
+    public List<Enterprise> getEnterprisesForUser(long userID) {
+        return null;
+    }
+
+    @Override
+    public List<User> getUsersForEnterprise(long enterpriseID) {
+        return null;
+    }
+
+    @Override
+    public List<JobOfferWithStatus> getJobOffersWithStatusForUser(long userId) {
+        return null;
+    }
+
+    @Override
+    public List<JobOfferStatusUserData> getJobOffersWithStatusUserData(long enterpriseID, int page, int pageSize, String status) {
+        return null;
+    }
+
+    @Override
+    public List<JobOfferStatusUserData> getAllJobOffersWithStatusUserData(long enterpriseID, int page, int pageSize) {
+        return null;
+    }
+
+    @Override
+    public List<JobOfferStatusEnterpriseData> getJobOffersWithStatusEnterpriseData(long userID, int page, int pageSize, String status) {
+        return null;
+    }
+
+    @Override
+    public List<JobOfferStatusEnterpriseData> getAllJobOffersWithStatusEnterpriseData(long userID, int page, int pageSize) {
+        return null;
+    }
+
+    @Override
+    public boolean alreadyContacted(long userID, long jobOfferID) {
+        return false;
+    }
+
+    @Override
+    public String getStatus(long userID, long jobOfferID) {
+        return null;
+    }
+
+    @Override
+    public void acceptJobOffer(long userID, long jobOfferID) {
+
+    }
+
+    @Override
+    public void rejectJobOffer(long userID, long jobOfferID) {
+
+    }
+
+    @Override
+    public void cancelJobOffer(long userID, long jobOfferID) {
+
+    }
+
+    @Override
+    public void cancelJobOfferForEveryone(long jobOfferID) {
+
+    }
+
+    @Override
+    public void closeJobOffer(long userID, long jobOfferID) {
+
+    }
+
+    @Override
+    public void closeJobOfferForEveryone(long jobOfferID) {
+
+    }
+
+    @Override
+    public long getContactsCountForEnterprise(long enterpriseID) {
+        return 0;
+    }
+
+    @Override
+    public long getContactsCountForUser(long userID) {
+        return 0;
+    }
+}
