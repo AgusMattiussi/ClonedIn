b71040b925cce4c42368bfc1266d6ed455f0f13d
Agustin Mattiussi <amattiussi@itba.edu.ar>
2022-11-12 19:09:09 -0300

    Implement ExperienceHibernateDao tests

--- paw-webapp/persistence/src/test/java/ar/edu/itba/paw/persistence/ExperienceHibernateDaoTest.java
+++ paw-webapp/persistence/src/test/java/ar/edu/itba/paw/persistence/ExperienceHibernateDaoTest.java
@@ -1,22 +1,23 @@
 package ar.edu.itba.paw.persistence;
 
-import ar.edu.itba.paw.interfaces.persistence.UserDao;
+import ar.edu.itba.paw.interfaces.persistence.ExperienceDao;
+import ar.edu.itba.paw.models.Category;
 import ar.edu.itba.paw.models.Experience;
 import ar.edu.itba.paw.models.User;
+import ar.edu.itba.paw.models.enums.Visibility;
 import ar.edu.itba.paw.persistence.config.TestConfig;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.sql.DataSource;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,67 +27,55 @@ import static org.junit.Assert.assertTrue;
 @ContextConfiguration(classes = TestConfig.class)
 @Rollback
 @Transactional
-public class ExperienceJdbcDaoTest {
-    /*private static final String EXPERIENCE_TABLE = "experiencia";
-    private static final String ID = "id";
-    private static final String USER_ID = "idUsuario";
-    private static final String FROM = "fechaDesde";
-    private static final String TO = "fechaHasta";
-    private static final String ENTERPRISE_NAME = "empresa";
-    private static final String POSITION = "posicion";
-    private static final String DESCRIPTION = "descripcion";
-
-    private static final String TEST_USER_EMAIL = "johnlennon@gmail.com";
+public class ExperienceHibernateDaoTest {
+    private static final String TEST_USER_EMAIL = "user@email.com";
+    private static final String TEST_CATEGORY_DESC = "testCategoryDescription";
+    private static final String EMPTY_FIELD = "";
     public static final int NEW_MONTH_FROM = 11;
     public static final int NEW_YEAR_FROM = 2000;
     public static final Integer NEW_MONTH_TO = 12;
     public static final Integer NEW_YEAR_TO = 2004;
-    private static final String NEW_ENTERPRISE_NAME = "Empresa 1";
-    private static final String NEW_POSITION = "Hokage";
-    private static final String NEW_DESCRIPTION = "El admin de la aldea";
-
-    private static final long FIRST_ID = 1;
+    private static final String NEW_ENTERPRISE_NAME = "newEnterprise";
+    private static final String NEW_POSITION = "newPosition";
+    private static final String NEW_DESCRIPTION = "newDescription";
+    private static final long ID = 1;
     private static final long EXISTING_USER_ID = 1;
-
-    private static final String EXISTING_ENTERPRISE_NAME = "Paw Inc.";
+    private static final String EXISTING_ENTERPRISE_NAME = "testEnterpriseName";
     public static final int EXISTING_MONTH_FROM = 11;
     public static final int EXISTING_YEAR_FROM = 2011;
     public static final Integer EXISTING_MONTH_TO = 12;
     public static final Integer EXISTING_YEAR_TO = 2012;
-    private static final String EXISTING_POSITION = "Ceo de Paw Inc.";
-    private static final String EXISTING_DESCRIPTION = "Era el CEO :)";
+    private static final String EXISTING_POSITION = "testPosition";
+    private static final String EXISTING_DESCRIPTION = "testDescription";
+    public static final long USER_EXPERIENCE_LIST_SIZE = 1;
 
-
-    @Autowired
-    private ExperienceJdbcDao dao;
-    @Autowired
-    private UserDao userDao;
+    @PersistenceContext
+    private EntityManager em;
 
     @Autowired
-    private DataSource ds;
+    private ExperienceDao dao;
 
-    private JdbcTemplate jdbctemplate;
+    private User testUser;
 
-    private User testUser;*/
-
-    @Test
-    public void dummyTest() {
-        assertTrue(true);
-    }
-
-    /*@Before
+    @Before
     public void setUp() {
-        jdbctemplate = new JdbcTemplate(ds);
-        testUser = userDao.findByEmail(TEST_USER_EMAIL).get();
+        Category testCategory = new Category(TEST_CATEGORY_DESC);
+        em.persist(testCategory);
+
+        testUser = new User(TEST_USER_EMAIL, EMPTY_FIELD, EMPTY_FIELD, EMPTY_FIELD, testCategory, EMPTY_FIELD,
+                EMPTY_FIELD, EMPTY_FIELD, Visibility.VISIBLE.getValue(), null);
+        em.persist(testUser);
+
+        Experience testExperience = new Experience(testUser, EXISTING_MONTH_FROM, EXISTING_YEAR_FROM, EXISTING_MONTH_TO, EXISTING_YEAR_TO, EXISTING_ENTERPRISE_NAME, EXISTING_POSITION, EXISTING_DESCRIPTION);
+        em.persist(testExperience);
     }
 
     @Test
     public void testCreate() {
-        final Experience newExperience = dao.create(testUser.getId(), NEW_MONTH_FROM, NEW_YEAR_FROM, NEW_MONTH_TO, NEW_YEAR_TO,
+        final Experience newExperience = dao.create(testUser, NEW_MONTH_FROM, NEW_YEAR_FROM, NEW_MONTH_TO, NEW_YEAR_TO,
                 NEW_ENTERPRISE_NAME, NEW_POSITION, NEW_DESCRIPTION);
 
         Assert.assertNotNull(newExperience);
-        Assert.assertEquals(1, newExperience.getUserId());
         Assert.assertEquals(NEW_MONTH_FROM, newExperience.getMonthFrom());
         Assert.assertEquals(NEW_YEAR_FROM, newExperience.getYearFrom());
         Assert.assertEquals(NEW_MONTH_TO, newExperience.getMonthTo());
@@ -94,17 +83,15 @@ public class ExperienceJdbcDaoTest {
         Assert.assertEquals(NEW_ENTERPRISE_NAME, newExperience.getEnterpriseName());
         Assert.assertEquals(NEW_POSITION, newExperience.getPosition());
         Assert.assertEquals(NEW_DESCRIPTION, newExperience.getDescription());
-
-        Assert.assertEquals(1, JdbcTestUtils.countRowsInTableWhere(jdbctemplate, EXPERIENCE_TABLE, POSITION + " = '" + NEW_POSITION + "'"));
     }
 
     @Test
     public void testFindById() {
-        final Optional<Experience> newExperience = dao.findById(FIRST_ID);
+        final Optional<Experience> newExperience = dao.findById(2);
 
         assertTrue(newExperience.isPresent());
-        Assert.assertEquals(FIRST_ID, newExperience.get().getId());
-        Assert.assertEquals(EXISTING_USER_ID, newExperience.get().getUserId());
+        Assert.assertEquals(2, (long) newExperience.get().getId());
+        Assert.assertEquals(2, (long) newExperience.get().getUserId().getId());
         Assert.assertEquals(EXISTING_MONTH_FROM, newExperience.get().getMonthFrom());
         Assert.assertEquals(EXISTING_YEAR_FROM, newExperience.get().getYearFrom());
         Assert.assertEquals(EXISTING_MONTH_TO, newExperience.get().getMonthTo());
@@ -115,12 +102,12 @@ public class ExperienceJdbcDaoTest {
     }
 
     @Test
-    public void testFindByUserID() {
-        final List<Experience> experienceList = dao.findByUserId(testUser.getId());
+    public void testFindByUser() {
+        final List<Experience> experienceList = dao.findByUser(testUser);
 
         Assert.assertNotNull(experienceList);
-        Assert.assertEquals(1, experienceList.size());
-        Assert.assertEquals(EXISTING_USER_ID, experienceList.get(0).getUserId());
+        Assert.assertEquals(USER_EXPERIENCE_LIST_SIZE, experienceList.size());
+        Assert.assertEquals(EXISTING_USER_ID, (long) experienceList.get(0).getUserId().getId());
         Assert.assertEquals(EXISTING_MONTH_FROM, experienceList.get(0).getMonthFrom());
         Assert.assertEquals(EXISTING_YEAR_FROM, experienceList.get(0).getYearFrom());
         Assert.assertEquals(EXISTING_MONTH_TO, experienceList.get(0).getMonthTo());
@@ -132,12 +119,10 @@ public class ExperienceJdbcDaoTest {
 
     @Test
     public void testDeleteExperience(){
-        final Experience newExperience = dao.create(testUser.getId(), NEW_MONTH_FROM, NEW_YEAR_FROM, NEW_MONTH_TO, NEW_YEAR_TO,
-                NEW_ENTERPRISE_NAME, NEW_POSITION, NEW_DESCRIPTION);
-        Optional<Experience> foundExperience = dao.findById(newExperience.getId());
-        assertTrue(foundExperience.isPresent());
-        dao.deleteExperience(newExperience.getId());
-        foundExperience = dao.findById(newExperience.getId());
-        Assert.assertFalse(foundExperience.isPresent());
-    }*/
+        Experience testExperience2 = new Experience(testUser, EXISTING_MONTH_FROM, EXISTING_YEAR_FROM, EXISTING_MONTH_TO, EXISTING_YEAR_TO, EXISTING_ENTERPRISE_NAME, EXISTING_ENTERPRISE_NAME, EXISTING_DESCRIPTION);
+        em.persist(testExperience2);
+        dao.deleteExperience(testExperience2.getId());
+        //TODO: fix assert
+        //Assert.assertNull(em.find(Experience.class, testExperience2.getId()));
+    }
 }
