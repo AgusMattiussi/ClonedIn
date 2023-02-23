c1815c82ee27cb4b7437dbb245c0100791f70626
malevasquez <male.vasquez@yahoo.com.ar>
2023-02-23 14:42:32 -0300

    Corrected minor font issue in login page

--- frontend/src/login.tsx
+++ frontend/src/login.tsx
@@ -13,7 +13,7 @@ function Login() {
         <div className="row justify-content-center mt-0">
             <div className="col-11 col-sm-9 col-md-7 col-lg-6 text-center p-0 mt-3 mb-2">
             <Card className="custom-card px-0 pt-4 pb-0 mt-3 mb-3">
-            <h2><strong>Welcome to ClonedIn!</strong></h2>
+            <h2><strong>Welcome to ClonedIN!</strong></h2>
             <div className="row">
                         <div className="col-md-12 mx-0">
     <Form className='msform'>
@@ -29,7 +29,7 @@ function Login() {
       </Form.Group>
       </div>
       {/* TODO: arreglar el metodo de link porque href es ilegal - funciona though*/}
-      <Button href="/jobs" variant="success" type="submit"> Log In </Button>
+      <Button href="/jobs" variant="success" type="submit"><strong>Log In</strong></Button>
       <p>No account yet? Choose your user type to register.</p>
     </Form>
     <div className="row">
