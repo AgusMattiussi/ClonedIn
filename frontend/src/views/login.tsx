218bff014c604ab425591109539804c6c78755a5
sanselmo <soanselmo@itba.edu.ar>
2023-03-29 18:18:21 -0300

    translation

--- frontend/src/views/login.tsx
+++ frontend/src/views/login.tsx
@@ -4,8 +4,10 @@ import Header from "../components/header"
 import Container from "react-bootstrap/esm/Container"
 import Form from "react-bootstrap/Form"
 import Card from "react-bootstrap/Card"
+import { useTranslation } from 'react-i18next';
 
 function Login() {
+  const { t } = useTranslation();
   return (
     <div>
       <Header />
@@ -15,42 +17,42 @@ function Login() {
             <div className="col-11 col-sm-9 col-md-7 col-lg-6 p-0 mt-3 mb-2">
               <Card className="custom-card px-0 pt-4 pb-0 mt-3 mb-3">
                 <h2 className="text-center p-0 mt-3 mb-2">
-                  <strong>Welcome to ClonedIN!</strong>
+                  <strong>{t('Welcome to ClonedIN!')}</strong>
                 </h2>
                 <div className="row">
                   <div className="col-md-12 mx-0">
                     <Form className="msform">
                       <div className="form-card">
                         <Form.Group className="mb-3" controlId="formBasicEmail">
-                          <Form.Control className="input" type="email" placeholder="Email" />
+                          <Form.Control className="input" type="email" placeholder={t('Email').toString()}/>
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicPassword">
-                          <Form.Control className="input" type="password" placeholder="Password" />
+                          <Form.Control className="input" type="password" placeholder={t('Password').toString()}/>
                         </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                          <Form.Check type="checkbox" label="remember me" />
+                        <Form.Group className="mb-3 rememberme" controlId="formBasicCheckbox">
+                          <Form.Check type="checkbox" label={t('Remember me').toString()} />
                         </Form.Group>
                       </div>
                       {/* TODO: arreglar el metodo de link porque href es ilegal - funciona though*/}
                       <Button href="/discoverJobs" variant="success" type="submit">
-                        <strong>Log In</strong>
+                        <strong>{t('Log In')}</strong>
                       </Button>
-                      <p>No account yet? Choose your user type to register.</p>
+                      <p>{t('No account yet?')}</p>
                     </Form>
                     <div className="row">
                       <div className="col">
                         {/* TODO: arreglar el metodo de link porque href es ilegal - funciona though*/}
-        <Button href="/registerUser" variant="success">
+                        <Button href="/registerUser" variant="success">
                           <Icon.Person size={40} />
                         </Button>
-                        <p>Register as a User</p>
+                        <p>{t("Register as a User")}</p>
                       </div>
                       <div className="col">
                         {/* TODO: arreglar el metodo de link porque href es ilegal - funciona though*/}
-        <Button href="/registerEnterprise" variant="success">
+                        <Button href="/registerEnterprise" variant="success">
                           <Icon.Building size={40} />
                         </Button>
-                        <p>Register as an Enterprise</p>
+                        <p>{t("Register as an Enterprise")}</p>
                       </div>
                     </div>
                   </div>
--- frontend/src/views/registerUser.tsx
+++ frontend/src/views/registerUser.tsx
@@ -5,6 +5,7 @@ import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
 import * as React from 'react';
 import {useState, useEffect} from 'react';
+import { useTranslation } from 'react-i18next';
 
 function RegisterUser() {
 
@@ -82,7 +83,7 @@ function RegisterUser() {
     register(email, password, repeatPassowrd, name, city, category, position, description, studiesLevel);
   };
 
-
+  const { t } = useTranslation();
   return (
     <div>
     <Header/>
@@ -91,70 +92,70 @@ function RegisterUser() {
         <div className="row justify-content-center mt-0">
             <div className="col-11 col-sm-9 col-md-7 col-lg-6 p-0 mt-3 mb-2">
             <Card className="custom-card px-0 pt-4 pb-0 mt-3 mb-3">
-            <h2 className="text-center p-0 mt-3 mb-2"><strong>Register</strong></h2>
-            <p>Make sure to fill all fields before advancing.</p>
+            <h2 className="text-center p-0 mt-3 mb-2"><strong>{t("Register")}</strong></h2>
+            <p> {t("Fill all fields")} </p>
             <div className="row">
                         <div className="col-md-12 mx-0">
     <Form className='msform' onSubmit={handleSubmit}>
         <div className="form-card">
-        <h2 className="fs-title">Basic Information</h2>
+        <h2 className="fs-title">{t("Basic Information")}</h2>
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Control className='input' type="email" placeholder="Email *"
+        <Form.Control className='input' type="email" placeholder={t("Email*").toString()}
           value={email} onChange={(e) => setEmail(e.target.value)} />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicName">
-        <Form.Control className='input' placeholder="Name *"
+        <Form.Control className='input' placeholder={t("Name*").toString()}
           value={name} onChange={(e) => setName(e.target.value)} />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Control className='input' type="password" placeholder="Password *"
+        <Form.Control className='input' type="password" placeholder={t("Password*").toString()}
           value={password} onChange={(e) => setPassword(e.target.value)} />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckPassword">
-        <Form.Control className='input' type="password" placeholder="Repeat Password *"
+        <Form.Control className='input' type="password" placeholder={t("Repeat Password*").toString()}
           value={repeatPassowrd} onChange={(e) => setRepeatPassowrd(e.target.value)} />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicLocation">
-        <Form.Control className='input' placeholder="Location"
+        <Form.Control className='input' placeholder={t("Location").toString()}
           value={city} onChange={(e) => setCity(e.target.value)} />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicPosition">
-        <Form.Control className='input' placeholder="Position"
+        <Form.Control className='input' placeholder={t("Current Position").toString()}
           value={position} onChange={(e) => setPosition(e.target.value)} />
       </Form.Group>
       <div className="d-flex mb-4">
-        <label className="area">Education Level</label>
-        <Form.Select className="selectFrom" aria-label="Default select example"
+        <label className="area">{t("Education Level")}</label>
+        <Form.Select className="selectFrom"
           value={studiesLevel} onChange={(e) => setStudiesLevel(e.target.value)}>
-          <option value="No-especificado">Choose</option>
-          <option value="Primario">Primary</option>
-          <option value="Secundario">Secondary</option>
-          <option value="Terciario">Terciary</option>
-          <option value="Graduado">Graduate</option>
-          <option value="Postgrado">Postgraduate</option>
+          <option value="No-especificado"> {t("No-especificado")} </option>
+          <option value="Primario"> {t("Primario")} </option>
+          <option value="Secundario"> {t("Secundario")} </option>
+          <option value="Terciario"> {t("Terciario")} </option>
+          <option value="Graduado"> {t("Graduado")} </option>
+          <option value="Postgrado"> {t("Postgrado")} </option>
         </Form.Select>
       </div>
       <div className="d-flex mb-4">
-        <label className="area">Job Category</label>
+        <label className="area">{t("Job Category")}</label>
           <Form.Select className="selectFrom" aria-label="Default select example"
           value={category} onChange={(e) => setCategory(e.target.value)}>
           {categoryList.map((categoryListItem: any) => (
-          <option key={categoryListItem.id} value={categoryListItem.name}>{categoryListItem.name}</option>
+          <option key={categoryListItem.id} value={categoryListItem.name}>{t(categoryListItem.name)}</option>
         ))}
         </Form.Select>
       </div>
       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Control placeholder='About Me' as="textarea" rows={3} 
+        <Form.Control placeholder={t("About Me").toString()} as="textarea" rows={3} 
           value={description} onChange={(e) => setDescription(e.target.value)} />
       </Form.Group>
       </div>
-      <p>(*) Fields are required.</p>
+      <p> {t("Fields required")} </p>
       {/* TODO: arreglar el metodo de link porque href es ilegal - funciona though*/}
-      <Button variant="success" type="submit"><strong>Register</strong></Button>
+      <Button variant="success" type="submit"><strong>{t("Register")}</strong></Button>
     </Form>
     <div className="row">
     <div className="col mt-2 mb-2">
-    <Button href="/login" variant="outline-secondary"><strong>return</strong></Button>
+    <Button href="/login" variant="outline-secondary"><strong>{t("Return")}</strong></Button>
     </div>
     </div>
     </div>
