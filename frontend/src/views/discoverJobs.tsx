2eb6d4fbe5953cef6293fa71310abade3cc9128c
Agustin Mattiussi <amattiussi@itba.edu.ar>
2023-03-29 17:40:13 -0300

    Add translations in discoverJobs view

--- frontend/src/views/discoverJobs.tsx
+++ frontend/src/views/discoverJobs.tsx
@@ -1,60 +1,63 @@
-import Button from 'react-bootstrap/Button';
-import * as Icon from 'react-bootstrap-icons';
-import Container from 'react-bootstrap/esm/Container';
-import Row from 'react-bootstrap/esm/Row';
-import Col from 'react-bootstrap/esm/Col';
+import Button from "react-bootstrap/Button"
+import * as Icon from "react-bootstrap-icons"
+import Container from "react-bootstrap/esm/Container"
+import Row from "react-bootstrap/esm/Row"
+import Col from "react-bootstrap/esm/Col"
 // import Card from 'react-bootstrap/Card';
-import Navigation from '../components/navbar'
-import Form from 'react-bootstrap/Form';
-import Dropdown from 'react-bootstrap/Dropdown';
-import { useState, useEffect } from 'react';
+import Navigation from "../components/navbar"
+import Form from "react-bootstrap/Form"
+import Dropdown from "react-bootstrap/Dropdown"
+import { useState, useEffect } from "react"
+import { useTranslation } from "react-i18next"
 
 function DiscoverJobs() {
+  const [users, setUsers] = useState<any[]>([])
+  useEffect(() => {
+    fetch("http://localhost:8080/webapp_war/users")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setUsers(data)
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
+  }, [])
 
-  const [users, setUsers] = useState<any[]>([]);
-   useEffect(() => {
-      fetch('http://localhost:8080/webapp_war/users')
-         .then((response) => response.json())
-         .then((data) => {
-            console.log(data);
-            setUsers(data);
-         })
-         .catch((err) => {
-            console.log(err.message);
-         });
-   }, []);
-
+  const { t } = useTranslation()
 
   return (
     <div>
-      <Navigation/>
+      <Navigation />
       <Container fluid>
         <Row className="align-items-start d-flex">
           <Col sm={2} className="sidebar">
             <Row className="search">
-              <h5 className="ml-2 mt-2">Search by:</h5>
+              <h5 className="ml-2 mt-2">{t("Search By")}</h5>
             </Row>
             <Row>
               <Form className="search">
                 <Form.Control
                   type="search"
-                  placeholder="Skill, position..."
+                  placeholder={t("Search Placeholder").toString()}
                   className="me-2"
                   aria-label="Search"
                 />
-                <div className='d-flex flex-wrap justify-content-center mt-2'>
-                <Button variant="outline-light" className='filterbtn' type="submit"><Icon.Search size={15}/></Button>
+                <div className="d-flex flex-wrap justify-content-center mt-2">
+                  <Button variant="outline-light" className="filterbtn" type="submit">
+                    <Icon.Search size={15} />
+                  </Button>
                 </div>
               </Form>
             </Row>
-            <br/>
+            <br />
             <Row className="search">
-              <h5>Filter by:</h5>
+              <h5>{t("Filter By")}</h5>
             </Row>
             <Row>
               <Dropdown>
                 <Dropdown.Toggle variant="light" id="dropdown-basic" className="dropdown">
-                  Category
+                  {t("Category")}
                 </Dropdown.Toggle>
                 <Dropdown.Menu>
                   <Dropdown.Item>Category 1</Dropdown.Item>
@@ -63,71 +66,64 @@ function DiscoverJobs() {
                 </Dropdown.Menu>
               </Dropdown>
             </Row>
-            <br/>
+            <br />
             <Row>
               <Dropdown>
                 <Dropdown.Toggle variant="light" id="dropdown" className="dropdown">
-                  Modality
+                  {t("Modality")}
                 </Dropdown.Toggle>
                 <Dropdown.Menu>
-                  <Dropdown.Item>Remoto</Dropdown.Item>
-                  <Dropdown.Item>Presencial</Dropdown.Item>
-                  <Dropdown.Item>Mixto</Dropdown.Item>
+                  <Dropdown.Item>{t("Home Office")}</Dropdown.Item>
+                  <Dropdown.Item>{t("On Site")}</Dropdown.Item>
+                  <Dropdown.Item>{t("Mixed")}</Dropdown.Item>
                 </Dropdown.Menu>
               </Dropdown>
             </Row>
-            <br/>
-            <Row className = "search">
-              <h5>Salary</h5>
+            <br />
+            <Row className="search">
+              <h5>{t("Salary")}</h5>
             </Row>
-            <Row className = "search">
-              <Form className="d-flex" >
-                <Form.Control
-                  type="search"
-                  placeholder="Min"
-                  className="me-2"
-                  aria-label="Search"
-                />
+            <Row className="search">
+              <Form className="d-flex">
+                <Form.Control type="search" placeholder="Min" className="me-2" aria-label="Search" />
                 -
-                <Form.Control
-                  type="search"
-                  placeholder="Max"
-                  className="ms-2"
-                  aria-label="Search"
-                />
+                <Form.Control type="search" placeholder="Max" className="ms-2" aria-label="Search" />
               </Form>
-              <br/>
-              <div className='d-flex flex-wrap justify-content-center mt-2'>
-              <Button variant="outline-light "className="filterbtn">Filter</Button>
+              <br />
+              <div className="d-flex flex-wrap justify-content-center mt-2">
+                <Button variant="outline-light " className="filterbtn">
+                  {t("Filter")}
+                </Button>
               </div>
             </Row>
-            <br/>
+            <br />
             <Row>
-            <div className='d-flex flex-wrap justify-content-center'>
-            <Button variant="outline-light "className="filterbtn">Clear Filters</Button>
-            </div>
+              <div className="d-flex flex-wrap justify-content-center">
+                <Button variant="outline-light " className="filterbtn">
+                  {t("Clear Filter")}
+                </Button>
+              </div>
             </Row>
           </Col>
           <Col sm={6} className="align-items-start d-flex mt-2 mr-2 mb-2">
             <Row>
-              <h3>Discover Jobs</h3>
+              <h3>{t("Discover Jobs")}</h3>
             </Row>
             <Row>
-            {
-            users.map((user) => {
-            return (
-                <div className="post-card" key={user.username}>
-                  <h2 className="post-title">{user.username}</h2>
-                  <p className="post-body">{user.self}</p>
-                </div>
-            );
-          })}
+              {users.map((user) => {
+                return (
+                  <div className="post-card" key={user.username}>
+                    <h2 className="post-title">{user.username}</h2>
+                    <p className="post-body">{user.self}</p>
+                  </div>
+                )
+              })}
             </Row>
           </Col>
         </Row>
       </Container>
     </div>
-  );
+  )
 }
 
-export default DiscoverJobs;
+export default DiscoverJobs
