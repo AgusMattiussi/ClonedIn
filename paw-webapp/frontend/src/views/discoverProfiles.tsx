267a5eb3662a1cdf164136a678eee920051bd51d
Agustin Mattiussi <amattiussi@itba.edu.ar>
2023-06-27 19:18:23 -0300

    Add filterProfilesSidebar component to discoverProfiles view

--- paw-webapp/frontend/src/views/discoverProfiles.tsx
+++ paw-webapp/frontend/src/views/discoverProfiles.tsx
@@ -1,13 +1,9 @@
-import Button from "react-bootstrap/Button"
-import * as Icon from "react-bootstrap-icons"
 import Container from "react-bootstrap/esm/Container"
 import Row from "react-bootstrap/esm/Row"
 import Col from "react-bootstrap/esm/Col"
 import Navigation from "../components/navbar"
-import Form from "react-bootstrap/Form"
-import CategoriesSelect from "../components/selects/categoriesSelect"
-import EducationLevelSelect from "../components/selects/educationLevelSelect"
 import ProfileCard from "../components/cards/profileCard"
+import FilterProfilesSideBar from "../components/sidebars/filterProfilesSideBar"
 import { useState, useEffect } from "react"
 import { useTranslation } from "react-i18next"
 
@@ -37,73 +33,7 @@ function DiscoverProfiles() {
       <Navigation isEnterprise={true} />
       <Container fluid>
         <Row className="align-items-start d-flex vh-100">
-          <Col sm={2} className="sidebar">
-            <Row className="search">
-              <h5 className="ml-2 mt-2">{t("Search By")}</h5>
-            </Row>
-            <Row>
-              <Form className="search">
-                <Form.Control
-                  type="search"
-                  placeholder={t("Search Profile Placeholder").toString()}
-                  className="me-2"
-                  aria-label="Search"
-                />
-                <div className="d-flex flex-wrap justify-content-center mt-2">
-                  <Button variant="outline-light" className="filterbtn" type="submit">
-                    <Icon.Search size={15} />
-                  </Button>
-                </div>
-              </Form>
-            </Row>
-            <br />
-            <Row className="search">
-              <h5>{t("Filter By")}</h5>
-            </Row>
-            <Row>
-              <CategoriesSelect />
-            </Row>
-            <br />
-            <Row>
-              <EducationLevelSelect />
-            </Row>
-            <Row className="search mt-2">
-              <div className="d-flex justify-content-center">
-                <h6>{t("Years Of Experience")}</h6>
-              </div>
-            </Row>
-            <Row className="search">
-              <Form className="d-flex">
-                <Form.Control
-                  type="search"
-                  placeholder={t("Minimum").toString()}
-                  className="me-2"
-                  aria-label="Search"
-                />
-                -
-                <Form.Control
-                  type="search"
-                  placeholder={t("Maximum").toString()}
-                  className="ms-2"
-                  aria-label="Search"
-                />
-              </Form>
-              <br />
-              <div className="d-flex flex-wrap justify-content-center mt-2">
-                <Button variant="outline-light " className="filterbtn">
-                  {t("Filter")}
-                </Button>
-              </div>
-            </Row>
-            <br />
-            <Row>
-              <div className="d-flex flex-wrap justify-content-center">
-                <Button variant="outline-light " className="filterbtn">
-                  {t("Clear Filter")}
-                </Button>
-              </div>
-            </Row>
-          </Col>
+          <FilterProfilesSideBar />
           <Col className="align-items-start d-flex flex-column mt-2 mr-2 mb-2">
             <Row>
               <h3>{t("Discover Profiles")}</h3>
