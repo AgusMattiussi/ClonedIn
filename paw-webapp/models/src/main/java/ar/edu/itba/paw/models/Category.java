3b5cdd09c638bcd47ffee040103e3098a44ea6b7
AMattiussi <amattiussi@itba.edu.ar>
2023-10-14 16:18:32 -0300

    Add user relevant info to userDTO

--- paw-webapp/models/src/main/java/ar/edu/itba/paw/models/Category.java
+++ paw-webapp/models/src/main/java/ar/edu/itba/paw/models/Category.java
@@ -45,7 +45,7 @@ public class Category {
             return false;
         }
         Category category = (Category) o;
-        return id == category.id && name.equals(category.name);
+        return id.equals(category.id) && name.equals(category.name);
     }
 
     @Override
--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/dto/UserDTO.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/dto/UserDTO.java
@@ -8,15 +8,34 @@ import java.net.URI;
 
 public class UserDTO {
 
-    private String username;
-    private URI self;
     //TODO: Completar con info relevante para usuarios
-    /*private  URI assignedIssues;
-    private URI reportedIssues;*/
+    private long id;
+    private String username;
+    private String name;
+    private String location;
+    private String currentPosition;
+    private String description;
+    private String educationLevel;
+    private int visibility;
+    private URI self;
+    private URI image;
+    private URI category;
+    private URI experiences;
+    private URI educations;
+    private URI skills;
+
+    //TODO: Rol?
 
     public static UserDTO fromUser(final UriInfo uriInfo, final User user) {
         final UserDTO dto = new UserDTO();
+        dto.id = user.getId();
         dto.username = user.getEmail();
+        dto.name = user.getName();
+        dto.location = user.getLocation();
+        dto.currentPosition = user.getCurrentPosition();
+        dto.description = user.getDescription();
+        dto.educationLevel = user.getEducation();
+        dto.visibility = user.getVisibility();
 
         //TODO: Revisar si hace falta eliminar el webapp_war para deployar
         final UriBuilder userUriBuilder = uriInfo.getBaseUriBuilder().replacePath("webapp_war/users").path(String.valueOf(user.getId()));
@@ -27,12 +46,68 @@ public class UserDTO {
         return dto;
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public String getUsername() {
         return username;
     }
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public void setCurrentPosition(String currentPosition) {
+        this.currentPosition = currentPosition;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getEducationLevel() {
+        return educationLevel;
+    }
+
+    public void setEducationLevel(String educationLevel) {
+        this.educationLevel = educationLevel;
+    }
+
+    public int getVisibility() {
+        return visibility;
+    }
+
+    public void setVisibility(int visibility) {
+        this.visibility = visibility;
     }
 
     public URI getSelf() {
@@ -43,21 +118,7 @@ public class UserDTO {
         this.self = self;
     }
 
-    /*public URI getAssignedIssues() {
-        return assignedIssues;
-    }
 
-    public void setAssignedIssues(URI assignedIssues) {
-        this.assignedIssues = assignedIssues;
-    }
-
-    public URI getReportedIssues() {
-        return reportedIssues;
-    }
-
-    public void setReportedIssues(URI reportedIssues) {
-        this.reportedIssues = reportedIssues;
-    }*/
 
     @Override
     public String toString() {
