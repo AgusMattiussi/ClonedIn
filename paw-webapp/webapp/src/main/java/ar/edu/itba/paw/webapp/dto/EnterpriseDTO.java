650d5baf6a2803d3e7fa064d6546ded2bc454224
Julian Sasso <jsasso@itba.edu.ar>
2023-10-14 16:58:20 -0300

    Update EnterpriseDTO

--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/dto/EnterpriseDTO.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/dto/EnterpriseDTO.java
@@ -8,21 +8,46 @@ import java.net.URI;
 
 public class EnterpriseDTO {
 
+    private long id;
     private String name;
+    private String email;
+    private String location;
+    //TODO: private URI category;
+    private String workers;
+    private int year;
+    private String link;
     private String description;
+    //TODO: private URI image;
     private URI self;
     //TODO: Completar con info relevante para empresa
 
 
     public static EnterpriseDTO fromEnterprise(final UriInfo uriInfo, final Enterprise enterprise) {
         final EnterpriseDTO dto = new EnterpriseDTO();
-        dto.name = enterprise.getEmail();
+        dto.id = enterprise.getId();
+        dto.name = enterprise.getName();
+        dto.email = enterprise.getEmail();
+        dto.location = enterprise.getLocation();
+        //TODO: dto.category = enterprise.getCategory();
+        dto.workers = enterprise.getWorkers();
+        dto.year = enterprise.getYear();
+        dto.link = enterprise.getLink();
         dto.description = enterprise.getDescription();
+        //TODO: dto.image = enterprise.getImage();
 
-        final UriBuilder userUriBuilder = uriInfo.getAbsolutePathBuilder().replacePath("webapp_war/enterprises").path(String.valueOf(enterprise.getId()));
+        final UriBuilder userUriBuilder = uriInfo.getAbsolutePathBuilder().replacePath("webapp_war/enterprises")
+                .path(String.valueOf(enterprise.getId()));
         dto.self = userUriBuilder.build();
 
         return dto;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -31,6 +56,46 @@ public class EnterpriseDTO {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getWorkers() {
+        return workers;
+    }
+
+    public void setWorkers(String workers) {
+        this.workers = workers;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
     }
 
     public String getDescription() {
