11b75bcf86fcdba02472eb9764758f7eb25eb0f0
Julian Sasso <jsasso@itba.edu.ar>
2023-12-03 20:38:04 -0300

    Implement SecurityValidator class

--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/security/SecurityValidator.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/security/SecurityValidator.java
@@ -1,17 +1,32 @@
 package ar.edu.itba.paw.webapp.security;
 
+import ar.edu.itba.paw.interfaces.services.EnterpriseService;
 import ar.edu.itba.paw.interfaces.services.UserService;
+import ar.edu.itba.paw.models.Enterprise;
+import ar.edu.itba.paw.models.enums.Visibility;
+import ar.edu.itba.paw.models.exceptions.HiddenProfileException;
 import ar.edu.itba.paw.models.exceptions.UserIsNotProfileOwnerException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import ar.edu.itba.paw.models.exceptions.UserNotFoundException;
 import org.springframework.stereotype.Component;
+import ar.edu.itba.paw.models.User;
 
 @Component
 public class SecurityValidator {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private EnterpriseService enterpriseService;
+
+    private String getAuthEmail() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(auth != null)
+            return auth.getName();
+        return null;
+    }
 
     private boolean canAccessProfile(long requesterID, long profileID) {
         if(requesterID != profileID)
@@ -20,12 +35,41 @@ public class SecurityValidator {
     }
 
     public boolean canAccessUserProfile(long profileID) {
-        String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        System.out.println("\n\n\n\n Chequeando acceso");
-        System.out.println("Requester: " + email);
-        System.out.println("ProfileID: " + profileID);
-        System.out.println("\n\n\n\n");
+        String email = getAuthEmail();
+        if(email == null)
+            return false;
         Long userID = userService.getIdForEmail(email).orElseThrow(UserNotFoundException::new);
         return canAccessProfile(userID, profileID);
+    }
+
+    public boolean canAccessEnterpriseProfile(long profileID) {
+        String email = getAuthEmail();
+        if(email == null)
+            return false;
+        Long enterpriseID = enterpriseService.getIdForEmail(email).orElseThrow(UserNotFoundException::new);
+        return canAccessProfile(enterpriseID, profileID);
+    }
+
+    public boolean isUserVisible(long userID){
+        User user = userService.findById(userID).orElseThrow(UserNotFoundException::new);
+        if(user.getVisibility() != Visibility.VISIBLE.getValue())
+            throw new HiddenProfileException();
+        return true;
+    }
+
+    public boolean isJobOfferOwner(long jobOfferId){
+        String email = getAuthEmail();
+        if(email == null)
+            return false;
+        Enterprise enterprise = enterpriseService.findByEmail(email).orElseThrow(UserNotFoundException::new);
+        return enterprise.isJobOfferOwner(jobOfferId);
+    }
+
+    public boolean isExperienceOwner(long experienceID){
+        String email = getAuthEmail();
+        if(email == null)
+            return false;
+        User user = userService.findByEmail(email).orElseThrow(UserNotFoundException::new);
+        return user.hasExperience(experienceID);
     }
 }
