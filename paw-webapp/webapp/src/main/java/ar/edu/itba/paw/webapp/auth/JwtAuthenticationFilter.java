4f412de82246ae6ab3b45fbbce8f1b0899163a18
Julian Sasso <jsasso@itba.edu.ar>
2023-12-05 13:03:59 -0300

    Implement 'Basic' header authentication

--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/auth/JwtAuthenticationFilter.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/auth/JwtAuthenticationFilter.java
@@ -2,6 +2,7 @@ package ar.edu.itba.paw.webapp.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,12 +14,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private static final String AUTH_HEADER = "Authorization";
     private static final String AUTH_HEADER_BEARER = "Bearer ";
-    private static final int JWT_POSITION = 7; // Appears after 'Bearer '
+    private static final String AUTH_HEADER_BASIC = "Basic ";
 
     @Autowired
     private final JwtHelper jwtHelper;
@@ -33,28 +36,46 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        final String authHeader = request.getHeader(AUTH_HEADER);
-        final String jwt;
-        final String userEmail;
+        String authHeader = request.getHeader(AUTH_HEADER);
 
-        if(authHeader == null || !authHeader.startsWith(AUTH_HEADER_BEARER)){
-            filterChain.doFilter(request, response);
-            return;
+        if(authHeader != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            if(authHeader.startsWith(AUTH_HEADER_BEARER)) {
+                jwtAuthentication(authHeader, request);
+            } else if(authHeader.startsWith(AUTH_HEADER_BASIC)){
+                basicAuthentication(authHeader, request);
+            }
         }
+        filterChain.doFilter(request, response);
+    }
 
-        jwt = authHeader.substring(JWT_POSITION);
-        userEmail = jwtHelper.extractUsername(jwt);
-        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
 
+    private void jwtAuthentication(String authHeader, HttpServletRequest request){
+        String jwt = authHeader.substring(7); // "Bearer ".length()
+        String userEmail = jwtHelper.extractUsername(jwt);
+        if (userEmail != null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
 
-            if(jwtHelper.isTokenValid(jwt, userDetails)){
+            if (jwtHelper.isTokenValid(jwt, userDetails)) {
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,
                         null, userDetails.getAuthorities());
                 authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
-        filterChain.doFilter(request, response);
+    }
+
+
+    private void basicAuthentication(String authHeader, HttpServletRequest request){
+        String basic = authHeader.substring(6); // "Basic ".length()
+
+        String credentials = new String(Base64.getDecoder().decode(basic), StandardCharsets.UTF_8);
+        String[] splitCredentials = credentials.split(":", 2);
+
+        String email = splitCredentials[0];
+        String password = splitCredentials[1];
+
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(email, password);
+        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authToken);
     }
 }
