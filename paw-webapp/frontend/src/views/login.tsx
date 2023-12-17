ab6cfb9ce2a3a8664df9a2a482d041093ff178f1
malevasquez <male.vasquez@yahoo.com.ar>
2023-12-16 23:17:57 -0300

    Began frontend testing for components

--- paw-webapp/frontend/src/views/login.tsx
+++ paw-webapp/frontend/src/views/login.tsx
@@ -1,7 +1,7 @@
 import Button from "react-bootstrap/Button"
 import * as Icon from "react-bootstrap-icons"
 import Header from "../components/header"
-import Container from "react-bootstrap/esm/Container"
+import Container from "react-bootstrap/Container"
 import Form from "react-bootstrap/Form"
 import Card from "react-bootstrap/Card"
 import { useTranslation } from "react-i18next"
