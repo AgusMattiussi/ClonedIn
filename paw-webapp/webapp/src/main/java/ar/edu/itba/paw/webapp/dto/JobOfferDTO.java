6c7e186e574e5b5f646e8afcc93f7507e4bb2256
Julian Sasso <jsasso@itba.edu.ar>
2024-01-08 17:37:07 -0300

    Implement links in JobOfferDTO

--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/dto/JobOfferDTO.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/dto/JobOfferDTO.java
@@ -18,15 +18,14 @@ public class JobOfferDTO {
     private static final String CATEGORIES_URL = "webapp_war/categories";
 
     private long id;
-    private URI enterprise;
-    private URI category;
     private String position;
     private String description;
     private double salary;
     private String modality;
     private String available;
-    private List<String> skills;
-    private URI self;
+    private List<String> skills; //TODO: Solucionar con SkillController y SkillDTO para buscar por skill
+    private JobOfferDTOLinks links;
+
 
     public static JobOfferDTO fromJobOffer(final UriInfo uriInfo, final JobOffer jobOffer) {
         final JobOfferDTO dto = new JobOfferDTO();
@@ -35,25 +34,10 @@ public class JobOfferDTO {
         dto.description = jobOffer.getDescription();
         dto.salary = jobOffer.getSalary().doubleValue();
         dto.modality = jobOffer.getModality();
-
-
         List<Skill> jobOfferSkills = jobOffer.getSkills();
         dto.skills = jobOfferSkills.stream().map(Skill::getDescription).collect(java.util.stream.Collectors.toList());
 
-        final UriBuilder jobOfferUriBuilder = uriInfo.getAbsolutePathBuilder()
-                .replacePath(JOB_OFFERS_URL)
-                .path(String.valueOf(jobOffer.getId()));
-        dto.self = jobOfferUriBuilder.build();
-
-        UriBuilder enterpriseUriBuilder = uriInfo.getAbsolutePathBuilder()
-                .replacePath(ENTERPRISES_URL)
-                .path(String.valueOf(jobOffer.getEnterprise().getId()));
-        dto.enterprise = enterpriseUriBuilder.build();
-
-        UriBuilder categoryUriBuilder = uriInfo.getAbsolutePathBuilder()
-                .replacePath(CATEGORIES_URL)
-                .path(String.valueOf(jobOffer.getCategory().getId()));
-        dto.category = categoryUriBuilder.build();
+        dto.links = new JobOfferDTOLinks(uriInfo, jobOffer);
 
         return dto;
     }
@@ -64,22 +48,6 @@ public class JobOfferDTO {
 
     public void setId(long id) {
         this.id = id;
-    }
-
-    public URI getEnterprise() {
-        return enterprise;
-    }
-
-    public void setEnterprise(URI enterprise) {
-        this.enterprise = enterprise;
-    }
-
-    public URI getCategory() {
-        return category;
-    }
-
-    public void setCategory(URI category) {
-        this.category = category;
     }
 
     public String getPosition() {
@@ -130,11 +98,62 @@ public class JobOfferDTO {
         this.skills = skills;
     }
 
-    public URI getSelf() {
-        return self;
+    public JobOfferDTOLinks getLinks() {
+        return links;
     }
 
-    public void setSelf(URI self) {
-        this.self = self;
+    public void setLinks(JobOfferDTOLinks links) {
+        this.links = links;
+    }
+
+    public static class JobOfferDTOLinks {
+
+        private URI self;
+        private URI enterprise;
+        private URI category;
+
+        public JobOfferDTOLinks() {
+        }
+
+        public JobOfferDTOLinks(final UriInfo uriInfo, final JobOffer jobOffer) {
+            final UriBuilder jobOfferUriBuilder = uriInfo.getAbsolutePathBuilder()
+                    .replacePath(JOB_OFFERS_URL)
+                    .path(String.valueOf(jobOffer.getId()));
+            this.self = jobOfferUriBuilder.build();
+
+            UriBuilder enterpriseUriBuilder = uriInfo.getAbsolutePathBuilder()
+                    .replacePath(ENTERPRISES_URL)
+                    .path(String.valueOf(jobOffer.getEnterprise().getId()));
+            this.enterprise = enterpriseUriBuilder.build();
+
+            UriBuilder categoryUriBuilder = uriInfo.getAbsolutePathBuilder()
+                    .replacePath(CATEGORIES_URL)
+                    .path(String.valueOf(jobOffer.getCategory().getId()));
+            this.category = categoryUriBuilder.build();
+        }
+
+        public URI getSelf() {
+            return self;
+        }
+
+        public void setSelf(URI self) {
+            this.self = self;
+        }
+
+        public URI getEnterprise() {
+            return enterprise;
+        }
+
+        public void setEnterprise(URI enterprise) {
+            this.enterprise = enterprise;
+        }
+
+        public URI getCategory() {
+            return category;
+        }
+
+        public void setCategory(URI category) {
+            this.category = category;
+        }
     }
 }
