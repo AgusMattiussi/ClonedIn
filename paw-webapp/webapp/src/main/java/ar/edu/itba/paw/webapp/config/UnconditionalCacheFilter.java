fb4ce642c0fbfbfb56644fb1589da8d264ae4e9d
Julian Sasso <jsasso@itba.edu.ar>
2024-01-16 20:35:44 -0300

    Update UnconditionalCacheFilter to cache only GET requests

--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/config/UnconditionalCacheFilter.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/config/UnconditionalCacheFilter.java
@@ -1,5 +1,7 @@
 package ar.edu.itba.paw.webapp.config;
 
+import com.sun.research.ws.wadl.HTTPMethods;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -7,13 +9,19 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class UnconditionalCacheFilter extends OncePerRequestFilter {
-    private static final int MAX_TIME = 31536000;
+    //TODO: Sacar de config
+    private static final int MAX_AGE = 4 * 60 * 60; // 4 hours
+    private static final String CACHE_CONTROL_VALUE = String.format("public, max-age=%d, immutable", MAX_AGE);
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        response.setHeader("Cache-Control", "public, max-age=" + MAX_TIME + ", immutable");
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        if(request.getMethod().equals("GET"))
+            response.setHeader(HttpHeaders.CACHE_CONTROL, CACHE_CONTROL_VALUE);
+
         filterChain.doFilter(request, response);
     }
 }
