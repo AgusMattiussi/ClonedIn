b4201eba992dd828f5d91167bdf4a83d912dfc21
Julian Sasso <jsasso@itba.edu.ar>
2024-01-17 16:40:36 -0300

    Refactor UnconditionalCacheFilter to StaticElementsCacheFilter

--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/filter/StaticElementsCacheFilter.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/filter/StaticElementsCacheFilter.java
@@ -1,7 +1,7 @@
-package ar.edu.itba.paw.webapp.config;
+package ar.edu.itba.paw.webapp.filter;
 
-import com.sun.research.ws.wadl.HTTPMethods;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,11 +12,24 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-public class UnconditionalCacheFilter extends OncePerRequestFilter {
+public class StaticElementsCacheFilter extends OncePerRequestFilter {
 
-    @Value("${unconditionalCache.maxAge}")
+    @Value("${cache.static-elements-max-age-seconds}")
     private int MAX_AGE;
-    private final String CACHE_CONTROL_VALUE = String.format("public, max-age=%d, immutable", MAX_AGE);
+    private final String CACHE_CONTROL_VALUE = cacheControlValue();
+
+
+    private String cacheControlValue() {
+        StringBuilder builder = new StringBuilder();
+        CacheControl basicCacheControl = CacheControl
+                .maxAge(MAX_AGE, TimeUnit.SECONDS)
+                .cachePublic();
+
+        builder.append(basicCacheControl.getHeaderValue())
+                .append(", immutable");
+
+        return builder.toString();
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
