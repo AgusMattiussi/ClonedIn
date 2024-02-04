343c0a6166ca299eda93db2974c65be4e64003bc
malevasquez <male.vasquez@yahoo.com.ar>
2024-02-04 10:56:39 -0300

    added mockedEnterprise and tests

--- paw-webapp/frontend/src/__tests__/components/profileEnterpriseCard.test.tsx
+++ paw-webapp/frontend/src/__tests__/components/profileEnterpriseCard.test.tsx
@@ -1,43 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import ProfileEnterpriseCard from "../../components/cards/profileEnterpriseCard";
+import { getEnterpriseMockedData } from "../../__mocks__/mockedData";
 
-const enterpriseLinks = {
-    self: "",
-    image: "logo.png",
-    category: "Technology"
-  }
-
-const categoryLinks = {
-    self: ""
-  }
-  
-
-const category = {
-    id: 155,
-    name: "Technology",
-    links: categoryLinks
-}
-
-const enterprise = {
-    id: 150,
-    name: "IBM",
-    email: "hr@ibm.com",
-    location: "CABA",
-    workers: "501-1000",
-    year: 1950,
-    website: "www.ibm.com",
-    description: "Global technology innovator, leading advances in AI, automation and hybrid cloud solutions that help businesses",
-    categoryInfo: category,
-    links: enterpriseLinks
-}
-
+const enterprise = getEnterpriseMockedData()
 
 const mockUsedNavigate = jest.fn();
 jest.mock('react-router-dom', () => ({
    ...jest.requireActual('react-router-dom'),
   useNavigate: () => mockUsedNavigate,
 }));
-
 
 describe('Test Job Offer Discover Card', ()=>{
 
@@ -46,13 +17,4 @@ describe('Test Job Offer Discover Card', ()=>{
         expect(screen.getByText(enterprise.name)).toBeInTheDocument()
     })
 
-    // test('Test that all information is visible', ()=>{
-    //     render(<ProfileEnterpriseCard enterprise={enterprise}/>)
-    //     expect(screen.getByText(enterprise.name)).toBeInTheDocument()
-    //     expect(screen.getByText(enterprise.location)).toBeInTheDocument()
-    //     expect(screen.getByText(enterprise.workers)).toBeInTheDocument()
-    //     expect(screen.getByText(enterprise.year)).toBeInTheDocument()
-    //     expect(screen.getByText(enterprise.website)).toBeInTheDocument()
-    //     expect(screen.getByText(enterprise.description)).toBeInTheDocument()
-    // })
 })
