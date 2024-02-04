9c37f9c68ec30588935bcf03f70e6d58baeebd28
sanselmo <soanselmo@itba.edu.ar>
2024-02-03 21:52:46 -0300

    add pagination test

---
+++ paw-webapp/frontend/src/__tests__/components/pagination.test.tsx
@@ -0,0 +1,28 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import Pagination from "../../components/pagination";
+
+let propsMap: { setter: any; pages: string; }
+
+const customPropsMap = (options = {}) => {
+    const map = {
+        setter: jest.fn(),
+        pages: "5"
+    }
+
+    return { ...map, ...options };
+};
+
+describe('Test Pagination', ()=>{
+    beforeEach(()=>{
+        propsMap = customPropsMap()
+    })
+
+    test('Test that all props are visible', ()=>{
+        render(<Pagination {...propsMap}/>)
+        const pages = propsMap.pages
+        pages.localeCompare((num) => {
+            expect(screen.getByText(`${num}`)).toBeInTheDocument()
+        })
+    })
+    
+})
