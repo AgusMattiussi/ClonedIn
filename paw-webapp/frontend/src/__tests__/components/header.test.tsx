9a0e46fad51e92effe2e5b766a58462de4248e91
sanselmo <soanselmo@itba.edu.ar>
2024-02-04 19:38:25 -0300

    minor fix

--- paw-webapp/frontend/src/__tests__/components/header.test.tsx
+++ paw-webapp/frontend/src/__tests__/components/header.test.tsx
@@ -1,6 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import Header from "../../components/header";
 
+const mockUsedNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+   ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockUsedNavigate,
+}));
+
 describe('Test Header', ()=>{
 
     test('Test that modal renders correctly', ()=>{
