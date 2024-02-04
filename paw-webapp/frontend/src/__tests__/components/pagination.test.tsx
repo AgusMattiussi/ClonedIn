13d6a182e6f0cc69a443b8572697974cddd50dd4
sanselmo <soanselmo@itba.edu.ar>
2024-02-04 15:02:35 -0300

    minor fix

--- paw-webapp/frontend/src/__tests__/components/pagination.test.tsx
+++ paw-webapp/frontend/src/__tests__/components/pagination.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Pagination from "../../components/pagination";
 
 let propsMap: { setter: any; pages: string; }
