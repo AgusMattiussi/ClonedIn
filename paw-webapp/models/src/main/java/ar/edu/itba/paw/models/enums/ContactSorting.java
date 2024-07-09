ce9c96e2e65c2dc2b6829256640f2aa042954a85
Julian Sasso <jsasso@itba.edu.ar>
2024-07-09 17:52:43 -0300

    Fix contact sorting by date

--- paw-webapp/models/src/main/java/ar/edu/itba/paw/models/enums/ContactSorting.java
+++ paw-webapp/models/src/main/java/ar/edu/itba/paw/models/enums/ContactSorting.java
@@ -1,30 +1,17 @@
 package ar.edu.itba.paw.models.enums;
 
 public enum ContactSorting {
-    ANY(0, ContactSorting.ANY_VALUE),
-    JOB_OFFER_POSITION(1, ContactSorting.JOB_OFFER_POSITION_VALUE),
-    USERNAME(2, ContactSorting.USERNAME_VALUE),
-    STATUS(3, ContactSorting.STATUS_VALUE),
-    DATE_ASC(4, ContactSorting.DATE_ASC_VALUE),
-    DATE_DESC(5, ContactSorting.DATE_DESC_VALUE);
+    ANY("any"),
+    JOB_OFFER_POSITION("jobOfferPosition"),
+    USERNAME("username"),
+    STATUS("status"),
+    DATE_ASC("dateAsc"),
+    DATE_DESC("dateDesc");
 
-    public static final String ANY_VALUE = "any";
-    public static final String JOB_OFFER_POSITION_VALUE = "jobOfferPosition";
-    public static final String USERNAME_VALUE = "username";
-    public static final String STATUS_VALUE = "status";
-    public static final String DATE_ASC_VALUE = "dateAsc";
-    public static final String DATE_DESC_VALUE = "dateDesc";
-
-    private final int value;
     private final String asString;
 
-    ContactSorting(int value, String asString) {
-        this.value = value;
+    ContactSorting(String asString) {
         this.asString = asString;
-    }
-
-    public int getValue() {
-        return value;
     }
 
     public String getAsString() {
@@ -35,7 +22,7 @@ public enum ContactSorting {
         if(sorting != null && !sorting.isEmpty()) {
             String sortingLowercase = sorting.toLowerCase();
             for (ContactSorting sortBy : ContactSorting.values()) {
-                if (sortBy.getAsString().equals(sortingLowercase)) {
+                if (sortBy.getAsString().toLowerCase().equals(sortingLowercase)) {
                     return sortBy;
                 }
             }
